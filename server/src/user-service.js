7d833e8893832886c726d37484450b02dce4d126
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2020-11-07 16:00:54 +0100

    Add pseudocode for login

--- server/src/user-service.js
+++ server/src/user-service.js
@@ -5,8 +5,22 @@ import makeSalt from './auth';
 import hashPassword from './auth';
 
 class UserService {
-  login(username: string, password: string) {
-    return null;
+  login(username: string, enteredPassword: string) {
+    return new Promise<number>((resolve, reject) => {
+      pool.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
+        if (error) return reject(error);
+        if (!results.affectedRows) reject(new Error('No row deleted'));
+
+        // console.log(results);
+        // const enteredHash = hashPassword(enteredPassword, results.salt)
+        // if (enteredHash == results.hash) {
+        //     resolve(user_id)
+        // }
+        // else {
+        //     reject(new Error("Wrong password"));
+        // }
+      });
+    });
   }
 
   register(username: string, password: string) {
