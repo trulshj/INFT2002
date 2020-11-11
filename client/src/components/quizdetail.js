5a601495cd78c0bf018541493c8e5c378d0e15ab
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2020-11-11 21:32:39 +0100

    fix Star rating onchange event

--- client/src/components/quizdetail.js
+++ client/src/components/quizdetail.js
@@ -6,112 +6,111 @@ import { createHashHistory, Route } from 'history';
 import { NavLink } from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 
-
-
 const history = createHashHistory(); // Use history.push(...) to programmatically change path
-
 
 /**
  * Component for getting details of a spesific quiz
- * 
+ *
  */
 export class QuizDetail extends Component<{ match: { params: { quizId: number } } }> {
-    quiz: Quiz = { quizId: 0, quizName: '', quizCategory: '' };
-    questions: QuizQuestion[] = [];
-    rating = 1
-   
-    render() {
-      console.log(this.quiz);
-      return (
-        <>
-          <Card title={this.quiz.quiz_name}>
-            {this.questions.map((question) => (
-              <Card>
-                <Row key={question.quizQuestionId}>
-                  <Column width={10}>
-                    <NavLink to={'/quizzes/' + question.quiz_id + '/' + question.quiz_question_id}>
-                      {question.question}
-                    </NavLink>
-                  </Column>
-                </Row>
-              </Card>
-            ))}
-          </Card>
-          <Card title="Rate quiz">
-          <ReactStars size={24} value={this.rating} onChange={(event) => {
-            quizService
-              .createRating(this.rating, this.props.match.params.quizId)
-              .then(alert(this.rating + " " + this.props.match.params.quizId))
-              .catch((error: Error) => Alert.danger('Error creating rating: ' + error.message));
-          }}
-        ></ReactStars>
-          </Card>
-          
-        </>
-      );
-    }
-  
-    mounted() {
-      quizService
-        .get(this.props.match.params.quizId)
-        .then((quiz) => (this.quiz = quiz))
-        .catch((error: Error) => Alert.danger('Error getting quiz: ' + error.message));
-      quizService
-        .getAllQuestionsInQuiz(this.props.match.params.quizId)
-        .then((quizQuestions) => (this.questions = quizQuestions))
-        .catch((error: Error) => Alert.danger('Error getting quiz questions: ' + error.message));
-    }
+  quiz: Quiz = { quizId: 0, quizName: '', quizCategory: '' };
+  questions: QuizQuestion[] = [];
+  rating = 1;
+
+  render() {
+    console.log(this.quiz);
+    return (
+      <>
+        <Card title={this.quiz.quiz_name}>
+          {this.questions.map((question) => (
+            <Card>
+              <Row key={question.quizQuestionId}>
+                <Column width={10}>
+                  <NavLink to={'/quizzes/' + question.quiz_id + '/' + question.quiz_question_id}>
+                    {question.question}
+                  </NavLink>
+                </Column>
+              </Row>
+            </Card>
+          ))}
+        </Card>
+        <Card title="Rate quiz">
+          <ReactStars
+            size={24}
+            value={this.rating}
+            onChange={(newRating) => {
+              this.rating = newRating;
+              quizService
+                .createRating(this.rating, this.props.match.params.quizId)
+                .then(alert(this.rating + ' ' + this.props.match.params.quizId))
+                .catch((error: Error) => Alert.danger('Error creating rating: ' + error.message));
+            }}
+          ></ReactStars>
+        </Card>
+      </>
+    );
   }
+
+  mounted() {
+    quizService
+      .get(this.props.match.params.quizId)
+      .then((quiz) => (this.quiz = quiz))
+      .catch((error: Error) => Alert.danger('Error getting quiz: ' + error.message));
+    quizService
+      .getAllQuestionsInQuiz(this.props.match.params.quizId)
+      .then((quizQuestions) => (this.questions = quizQuestions))
+      .catch((error: Error) => Alert.danger('Error getting quiz questions: ' + error.message));
+  }
+}
 
 /**
  * Component for getting details of questions
  */
 export class QuestionDetail extends Component<{ match: { params: { quizQuestionId: number } } }> {
-    question: QuizQuestion = { quizQuestionId: 0, quizId: 0, question: '' };
-    questionOption: QuizQuestionOption[] = [];
-    questionOptionCorrect: QuizQuestionOption[] = [];
-  
-    render() {
-      console.log(this.question);
-      return (
-        <>
-          <Card title={this.question.question}>
-            {this.questionOption.map((option) => (
+  question: QuizQuestion = { quizQuestionId: 0, quizId: 0, question: '' };
+  questionOption: QuizQuestionOption[] = [];
+  questionOptionCorrect: QuizQuestionOption[] = [];
+
+  render() {
+    console.log(this.question);
+    return (
+      <>
+        <Card title={this.question.question}>
+          {this.questionOption.map((option) => (
+            <Card>
+              <Row key={option.quizQuestionOptionId}>
+                <Column width={10}>{option.question_answer}</Column>
+              </Row>
+            </Card>
+          ))}
+          <Card title="Correct Answer(s)">
+            {this.questionOptionCorrect.map((option) => (
               <Card>
                 <Row key={option.quizQuestionOptionId}>
                   <Column width={10}>{option.question_answer}</Column>
                 </Row>
               </Card>
             ))}
-            <Card title="Correct Answer(s)">
-              {this.questionOptionCorrect.map((option) => (
-                <Card>
-                  <Row key={option.quizQuestionOptionId}>
-                    <Column width={10}>{option.question_answer}</Column>
-                  </Row>
-                </Card>
-              ))}
-            </Card>
           </Card>
-        </>
-      );
-    }
-  
-    mounted() {
-      quizService
-        .getQuestion(this.props.match.params.quizQuestionId)
-        .then((question) => (this.question = question))
-        .catch((error: Error) => Alert.danger('Error getting question: ' + error.message));
-      quizService
-        .getQuestionOption(this.props.match.params.quizQuestionId)
-        .then((questionOption) => (this.questionOption = questionOption))
-        .catch((error: Error) => Alert.danger('Error getting question options: ' + error.message));
-      quizService
-        .getQuestionOptionCorrect(this.props.match.params.quizQuestionId)
-        .then((questionOptionCorrect) => (this.questionOptionCorrect = questionOptionCorrect))
-        .catch((error: Error) =>
-          Alert.danger('Error getting correct question option: ' + error.message),
-        );
-    }
+        </Card>
+      </>
+    );
   }
-  
+
+  mounted() {
+    quizService
+      .getQuestion(this.props.match.params.quizQuestionId)
+      .then((question) => (this.question = question))
+      .catch((error: Error) => Alert.danger('Error getting question: ' + error.message));
+    quizService
+      .getQuestionOption(this.props.match.params.quizQuestionId)
+      .then((questionOption) => (this.questionOption = questionOption))
+      .catch((error: Error) => Alert.danger('Error getting question options: ' + error.message));
+    quizService
+      .getQuestionOptionCorrect(this.props.match.params.quizQuestionId)
+      .then((questionOptionCorrect) => (this.questionOptionCorrect = questionOptionCorrect))
+      .catch((error: Error) =>
+        Alert.danger('Error getting correct question option: ' + error.message),
+      );
+  }
+}
