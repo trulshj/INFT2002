47be92a15c48cf1dee22f5c30e1e285fa522a030
sverre259 <sverre259@gmail.com>
2020-11-15 20:02:23 +0100

    Play

--- client/src/components/quizplay.js
+++ client/src/components/quizplay.js
@@ -1,17 +1,138 @@
+// @flow
+
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { Card, Alert, Row, Column, Form, Button, NavBar } from '../widgets';
 import quizService, { type Quiz, type Category, type QuizQuestion } from '../quiz-service';
 import { createHashHistory, Route } from 'history';
 import { NavLink } from 'react-router-dom';
-
-
+import { StarRating } from '../rating';
 
 const history = createHashHistory(); // Use history.push(...) to programmatically change path
 
-/**
- * Component for playing quizzes
- * TODO:
- *  - Everything
- */
-export class QuizPlay extends Component {}
+export class QuizPlay extends Component<{ match: { params: { quizId: number } } }> {
+    quiz: Quiz = { quizId: 0, quizName: '', quizCategory: '' };
+    question: QuizQuestion = { quizQuestionId: 0, quizId: 0, question: '' };
+    questionOption: QuizQuestionOption[] = [];
+    questions: QuizQuestion[] = [];
+    
+  render() {
+    return (
+      <>       
+        {this.questions.map((question) => (        
+          <Row key={question.quizQuestionId}>               
+            <Column width={5}>                                                                                    
+              <PlayOptions quizQuestionId={question.quiz_question_id} />
+            </Column>              
+            <Column>
+            </Column>
+          </Row>                      
+        ))}          
+          <Button.Success         
+          >
+            Done
+          </Button.Success>
+      </>
+    );
+  }
+    
+      mounted() {
+        quizService
+      .get(this.props.match.params.quizId)
+      .then((quiz) => (this.quiz = quiz))
+      .catch((error: Error) => Alert.danger('Error getting quiz: ' + error.message));
+    quizService
+      .getAllQuestionsInQuiz(this.props.match.params.quizId)
+      .then((quizQuestions) => (this.questions = quizQuestions))
+      .catch((error: Error) => Alert.danger('Error getting quiz questions: ' + error.message));      
+      }
+}
+
+export class PlayOptions extends Component {
+  question: QuizQuestion = { quizQuestionId: 0, quizId: 0, question: '' };
+  questionOption: QuizQuestionOption[] = [];
+  questionOptionCorrect: QuizQuestionOption[] = [];
+  questionAnswers: QuizQuestionOption[] = [];
+
+  render() {
+    console.log();
+    return (
+      <>
+        <Card title={this.question.question}>
+          {this.questionOption.map((option) => (
+            <Card>
+              <Row key={option.quizQuestionOptionId}>
+                <Column width={10}>{option.question_answer}</Column>
+                <Form.Checkbox
+                  checked = {(this.question_answer)}
+                  onChange={(event) => (this.question_answer = event.currentTarget.checked)}/>
+                  
+              </Row>
+            </Card>
+          ))}
+        </Card>
+      </>
+    );
+  }
+
+  mounted() {
+    quizService
+      .getQuestion(this.props.quizQuestionId)
+      .then((question) => (this.question = question))
+      .catch((error: Error) => Alert.danger('Error getting question: ' + error.message));
+    quizService
+      .getQuestionOption(this.props.quizQuestionId)
+      .then((questionOption) => (this.questionOption = questionOption))
+      .catch((error: Error) => Alert.danger('Error getting question options: ' + error.message));
+    quizService
+      .getQuestionOptionCorrect(this.props.quizQuestionId)
+      .then((questionOptionCorrect) => (this.questionOptionCorrect = questionOptionCorrect))
+      .catch((error: Error) =>
+        Alert.danger('Error getting correct question option: ' + error.message),
+      );
+  }
+}
+
+export class PlayOptionAnswer extends Component {
+  question: QuizQuestion = { quizQuestionId: 0, quizId: 0, question: '' };
+  questionOption: QuizQuestionOption[] = [];
+  questionOptionCorrect: QuizQuestionOption[] = [];
+
+
+  render() {
+    console.log(this.props.quizQuestionId);
+    return (
+      <>
+        <Card title={this.question.question}>
+          {this.questionOption.map((option) => (
+            <Card>
+              <Row key={option.quizQuestionOptionId}>
+                <Column width={10}>{option.question_answer}</Column>
+                <Form.Checkbox
+                checked={option.is_correct}
+                onChange={(event) => (option.is_correct = event.currentTarget.checked)}/>
+              </Row>
+            </Card>
+          ))}
+        </Card>
+      </>
+    );
+  }
+
+  mounted() {
+    quizService
+      .getQuestion(this.props.quizQuestionId)
+      .then((question) => (this.question = question))
+      .catch((error: Error) => Alert.danger('Error getting question: ' + error.message));
+    quizService
+      .getQuestionOption(this.props.quizQuestionId)
+      .then((questionOption) => (this.questionOption = questionOption))
+      .catch((error: Error) => Alert.danger('Error getting question options: ' + error.message));
+    quizService
+      .getQuestionOptionCorrect(this.props.quizQuestionId)
+      .then((questionOptionCorrect) => (this.questionOptionCorrect = questionOptionCorrect))
+      .catch((error: Error) =>
+        Alert.danger('Error getting correct question option: ' + error.message),
+      );
+  }
+}
